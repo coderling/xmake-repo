12d28f1f6709eaa30f990c30c4cc47c42b4a9456
coderling <coderling@gmail.com>
2022-11-07 22:36:53 +0800

    init xmake utils

--- packages/x/xmake-utils/xmake.lua
+++ packages/x/xmake-utils/xmake.lua
@@ -1,5 +1,5 @@
 package("xmake-utils")
-    add_urls("https://github.com/coderling/xmake_utils.git")
+    add_urls("https://github.com/coderling/xmake_utils.git main")
     on_install(function(package)
         print("project dir: $(projectdir)")
         print("cur dir: $(curdir)")
