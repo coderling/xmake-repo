5153786476e85027864cd2feebadd19213a57a38
coderling <coderling@gmail.com>
2022-11-08 14:33:25 +0800

    add install script

--- packages/l/LPrimitive/xmake.lua
+++ packages/l/LPrimitive/xmake.lua
@@ -1,2 +1,7 @@
 package("LPrimitive")
     add_urls("https://github.com/coderling/LPrimitive.git")
+    on_install(function(package)
+        local configs = {}
+        configs["build-tests"] = false
+        import("package.tools.xmake").install(package, configs)
+    end)
