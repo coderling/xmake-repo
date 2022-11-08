c5687fb5a5cbe0e31d4d752103c3fe302da37387
coderling <coderling@gmail.com>
2022-11-08 14:57:05 +0800

    add log

--- packages/l/LPrimitive/xmake.lua
+++ packages/l/LPrimitive/xmake.lua
@@ -1,6 +1,7 @@
 package("LPrimitive")
     add_urls("https://github.com/coderling/LPrimitive.git")
     on_install(function(package)
+        print("on install LPrimitive")
         local configs = {}
         configs["build-tests"] = false
         import("package.tools.xmake").install(package, configs)
