ca28048a58d534ca8c398e6679c9fe17b0ac53b4
coderling <coderling@gmail.com>
2022-11-08 16:22:53 +0800

    package ptimitive

--- packages/p/primitive/xmake.lua
+++ packages/p/primitive/xmake.lua
@@ -7,16 +7,6 @@ package("primitive")
         import("package.tools.xmake").install(package, configs)
     end)
 
-    on_fetch(function (package)
-        local result = {}
-        local libfiledir = (package:config("shared") and package:is_plat("windows", "mingw")) and "bin" or "lib"
-        result.links = "primitive"
-        result.linkdirs = package:installdir("lib")
-        result.includedirs = package:installdir("include")
-        result.libfiles = path.join(package:installdir(libfiledir), "primitive.lib")
-        return result
-    end)
-
     on_test(function (package)
         assert(package:has_cxxincludes("Align.hpp"))
     end)
